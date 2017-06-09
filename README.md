2e210b9ab21d7cdccd29f8bdd78ef6986120a314
Brian Leffew <Brian@MacBook-Pro-3.local>
2017-06-09 11:20:04 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,64 +1,45 @@
-# Token SOFA App
+# Token_Broadcast
 
-This repo helps you build a [Token app](https://www.tokenbrowser.com) in Javascript.
+<br>
+<h2>Abstract:</h2>
+<p>The <b>vision</b> for Token_Broadcast is simply:</p>
+“To allow owners of AdSpaces to create engaging marketplaces where advertisers compete for the attention of users in a way which is transparent, non-discriminatory, and programmatically verifiable.”
 
-The sample bot can:
+---
+<br>
+<p>For the <b><a href='https://www.tokenbrowser.com/'>June 2017 Token Hackathon</a></b>, Brian Leffew and I elected to assemble a ‘bot’ on the Token Browser named “Token_Broadcast”.</p>
+<p>The bot implements an open-market bidding mechanism, allowing users to compete for the transmission of message onto an AdSpace.</p>
+<p>At the end of the time interval, the user with the highest bid wins, and gets their message pushed onto both the AdSpace and other bidders. Simultaneously, a smart-contract reimburses the non-winners with their original funds.</p>
+<p>While the message could be easily redesigned to be pushed onto any external network, our proof-of-concept MVP uses a Twitter handle, <a href='https://twitter.com/Token_Broadcast'>@Token_Broadcast</a>, to push the winning message onto the Twitterverse</p>
+<br>
 
-* send messages
-* send and request money
-* create simple UI for buttons and menus
-* store sessions and state for each user
+---
 
-TODO
+<br>
+<br>
+<h3>The mechanics of the UX are rather simple:</h3>
+<ol>
+  <li>First, the user initiates a conversation with Broadcast_Bot on Token, using any ol’ string to get the conversation rolling</li>
+  <li>Next, Broadcast_Bot follows up with an introduction, then uses SOFA UI elements to ask the user to either bid, donate, or display</li>
+  <li>If the User decides to bid, she enters a value amount, which is listed in local currency and recorded in Ethereum.</li>
+  <li>If the bid is valid (i.e.- greater than the previous highest bid), the bot accepts it, asking the user “What is the message you’d like to Broadcast?”</li>
+  <li>After the bot recieves the submission, it is recorded and stored as a JSON object</li>
+  <li>If the bid associated with message remains the highest at the end of the cycle — it is pushed through the Twitter API and tweeted out</li>
+</ol>
+<br>
+<br>
 
-* sending image messages
-* creating web view UIs
+---
 
-## Launch your own Token app in 5 minutes
+<h2>Example UI Flow</h2>
+<p align="center">
+  <img src="https://github.com/bleffew99/Token_Broadcast/blob/master/UI%20Flow%20Assets/IMG_3567.PNG" width="350"/>
+  <img src="https://github.com/bleffew99/Token_Broadcast/blob/master/UI%20Flow%20Assets/IMG_3578.PNG" width="350"/>
+  <img src="https://github.com/bleffew99/Token_Broadcast/blob/master/UI%20Flow%20Assets/IMG_3572.PNG" width="350"/>
+  <img src="https://github.com/bleffew99/Token_Broadcast/blob/master/UI%20Flow%20Assets/IMG_3574.PNG" width="350"/>
+  <img src="https://github.com/bleffew99/Token_Broadcast/blob/master/UI%20Flow%20Assets/IMG_3577.PNG" width="350"/>
+</p>
 
-Read our [guide to creating a Token app](http://developers.tokenbrowser.com/docs/creating-a-token-app).
+---
 
-When ready, fork this repo and deploy it to Heroku.
-
-[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
-
-Then check out [`src/bot.js`](src/bot.js) to start changing the bot logic.
-
-## Running locally with Docker
-
-You can run the project locally with
-
-```
-docker-compose up
-```
-
-If any new depencies are added you can rebuild the project with
-
-```
-docker-compose build
-```
-
-To reset the postgres database in your dev environment you can use
-
-```
-docker-compose down -v
-```
-
-## Architecture
-
-Deploying a Token app requires a few processes to run:
-
-* **token-headless-client**<br>
-  This is a client we provide (similar to the iOS or Android client) that provides a wrapper around the Token backend services. It also handles end-to-end encrypting all messages using the Signal protocol. It is written in Java and runs in the background, proxying all the requests to amd from your bot.
-* **redis**<br>
-  We use redis pub/sub to provide a connection between the token-headless-client and your bot.
-* **bot.js**<br>
-  This is where all your app logic lives.
-* **postgres**<br>
-  Postgres is used to store session data so you can persist state for each user who talks to your bot (similar to cookies in a web browser).
-
-![diagram](docs/images/app-architecture.png)
-
-## See also
-
-* [https://www.tokenbrowser.com]
+<h5><i>For a full description of the Token_Broadcast bot, check out our <b><a href='https://medium.com/@kleffew/tokenbroadcast-cf9963b54d99'>press-release</a></b>, found at https://medium.com/@kleffew/tokenbroadcast-cf9963b54d99</i></h5>
--- src/bot.js
+++ src/bot.js
@@ -2,44 +2,135 @@ const Bot = require('./lib/Bot')
 const SOFA = require('sofa-js')
 const Fiat = require('./lib/Fiat')
 
+// config time
+var startDate = new Date();
+var startMinutes = startDate.getMinutes();
+
+var date
+var minutes
+
+// configure twitter stuff
+const Twit = require('twit')
+
+
+
 let bot = new Bot()
+const twitterBot = new Twit({
+  consumer_key:         '7zNXlyxBkpfD7kTYAVj66Lxv1',
+  consumer_secret:      'scPEFtm19NTbNnfGc6Frh0iOo3vNN1UEGw43XqlsEZ8pdfnn2Z',
+  access_token:         '872565381361274881-mGQ8UfexwstCn6WKIEXaREWy2gqTSqg',
+  access_token_secret:  'vSbfEch2SgbGi7oF1RBdtVuEEz5WBIJSYfKrIxNjbQEI0',
+})
 
-// ROUTING
+// downoad current bid, set to local value
+var bids = [{
+  bid: 0.005,
+  tweet: 'No one bidded on this message :(',
+  tokenId:''
+}];
+var highestBid
 
+var newTweet = ''
+var newBid = 0
+var newTokenId = ''
+
+
+// check time, then send tweet if time has run out
 bot.onEvent = function(session, message) {
-  switch (message.type) {
-    case 'Init':
-      welcome(session)
-      break
-    case 'Message':
-      onMessage(session, message)
-      break
-    case 'Command':
-      onCommand(session, message)
-      break
-    case 'Payment':
-      onPayment(session, message)
-      break
-    case 'PaymentRequest':
-      welcome(session)
-      break
-  }
+    switch (message.type) {
+      case 'Init':
+        session.set('userBid', userBid)
+        welcome(session)
+        break
+      case 'Message':
+        onMessage(session, message)
+        break
+      case 'Command':
+        onCommand(session, message)
+        break
+      case 'Payment':
+        onPayment(session, message)
+        break
+      case 'PaymentRequest':
+        welcome(session)
+        break
+    }
 }
 
 function onMessage(session, message) {
-  welcome(session)
+  // check if time to bid has run out
+  highestBid = findHighestBid()
+  console.log(highestBid)
+  date = new Date()
+  minutes = date.getMinutes()
+
+  if((minutes - 5) % 60 >= startMinutes) {
+    startMinutes = minutes
+      // user lost bid
+      session.reply("Times up! The winning message was \"" + highestBid.tweet + "\"!")
+      // refund money
+      var bidsToRefund = 0
+      for (var i = 0; i < bids.length; i++) {
+        if (bids[i].tokenId == session.get('tokenId') && bids[i].bid < highestBid.bid) {
+          bidsToRefund += bids[i].bid
+        }
+      }
+      if (bidsToRefund > 0)
+        session.sendEth(bidsToRefund)
+      tweet()
+    }
+  // set new message
+  if (session.get('askingForMessage')) {
+    session.set('askingForMessage', false)
+    // update object for session
+    newTweet = message.body
+    newTokenId = session.get('tokenId')
+    // then add to array of bids
+    bids.push({
+      bid: newBid,
+      tweet: newTweet,
+      tokenId: newTokenId
+    })
+
+    session.reply('New message is now "' + newTweet + '"')
+    highestBid = findHighestBid()
+    welcome(session)
+  }
+
+  // set new bid
+  else if (session.get('askingForBid')) {
+    //make sure bid is valid
+
+    if (parseFloat(message.body) > highestBid.bid && !isNaN(message.body)) {
+      // all good
+      session.set('newBid', parseFloat(message.body))
+      session.set('askingForBid', false)
+      session.requestEth(message.body)
+    }
+    else {
+      // not good
+      session.reply("I'm sorry, but the bid you entered was invalid. (Must be only number)")
+      session.set('askingForBid', false)
+    }
+  }
+
+  // else normal message
+  else {
+    welcome(session)
+  }
 }
 
 function onCommand(session, command) {
+  highestBid = findHighestBid()
   switch (command.content.value) {
-    case 'ping':
-      pong(session)
-      break
-    case 'count':
-      count(session)
+    case 'bid':
+      bid(session)
       break
     case 'donate':
       donate(session)
+      break
+    case 'display':
+      display(session)
       break
     }
 }
@@ -57,46 +148,81 @@ function onPayment(session, message) {
     // handle payments sent to the bot
     if (message.status == 'unconfirmed') {
       // payment has been sent to the ethereum network, but is not yet confirmed
-      sendMessage(session, `Thanks for the payment! 🙏`);
+      // ask for tweet
+      newBid = session.get('newBid')
+      getTweet(session)
     } else if (message.status == 'confirmed') {
       // handle when the payment is actually confirmed!
+      // ask user for tweet
+
     } else if (message.status == 'error') {
       sendMessage(session, `There was an error with your payment!🚫`);
     }
   }
 }
 
-// STATES
+// askingForMessageS
 
 function welcome(session) {
-  sendMessage(session, `Hello Token!`)
+  var minutesLeft = 5 - ((minutes + 5) % 60 - (startMinutes + 5) % 60)
+  session.reply(`Hi, welcome to Token_Broadcast!`)
+  session.reply('My function is to create an open and free marketplace where users can bid to get their message pushed to an AdSpace on Twitter')
+  sendMessage(session, 'The current bid is currently ' + highestBid.bid + 'ETH' + " with " + minutesLeft + " minutes left")
 }
 
-function pong(session) {
-  sendMessage(session, `Pong`)
+// sends new bid for tweet
+function bid(session) {
+  session.set('askingForBid', true)
+  session.reply('Current bid is ' + highestBid.bid + " ETH. Enter a new bid.")
 }
 
-// example of how to store state on each user
-function count(session) {
-  let count = (session.get('count') || 0) + 1
-  session.set('count', count)
-  sendMessage(session, `${count}`)
+// get new tweet from user, update current bid
+function getTweet(session) {
+  session.set('askingForMessage', true)
+  session.reply("What is your message?")
 }
 
-function donate(session) {
-  // request $1 USD at current exchange rates
-  Fiat.fetch().then((toEth) => {
-    session.requestEth(toEth.USD(1))
+// display current bid and message
+function display(session) {
+  session.reply("Current bid is " + highestBid.bid)
+  session.reply('Current tweet is "' + highestBid.tweet + '"')
+}
+
+// tweet out current message
+function tweet(session) {
+
+  // reset bid
+
+
+  twitterBot.post('statuses/update', { status: highestBid.tweet }, function(err, data, response) {
+  console.log(data)
   })
+  // reset bids
+  bids = [{
+    bid: 0.005,
+    tweet: 'No one bidded on this message :(',
+    tokenId:''
+  }];
+
 }
 
 // HELPERS
+function findHighestBid() {
+  var currhighestbid = bids[0]
+  for (var i = 0; i < bids.length; i++) {
+    if (bids[i].bid > currhighestbid.bid) {
+      currhighestbid = bids[i]
+    }
+  }
+  return currhighestbid
+}
+
 
 function sendMessage(session, message) {
   let controls = [
-    {type: 'button', label: 'Ping', value: 'ping'},
-    {type: 'button', label: 'Count', value: 'count'},
-    {type: 'button', label: 'Donate', value: 'donate'}
+    {type: 'button', label: 'Bid', value: 'bid'},
+    {type: 'button', label: 'Top Offer', value: 'display'},
+    {type: 'button', label: 'Twitter', action: 'Webview::https://twitter.com/Token_Broadcast'}
   ]
   session.reply(SOFA.Message({
     body: message,
